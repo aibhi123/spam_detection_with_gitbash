1b2846fc3a8c74f9b5f91988607b998a1ea48342
unknown <aibhisarkar123@gmail.com>
2024-09-15 18:49:33 +0530

    about page

---
+++ src/About.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './Css/radio.css'
+import './Css/Style.css'
+import './Css/bootstrap.min.css'
+// External JS scripts are generally not needed in React.
+// Avoid including JQuery.js and Bootstrap.js. Use React-specific libraries instead.
+
+function About() {
+  
+    // Add logic for handling the spam detection based on selected algorithm and text input.
+
+  return (
+    <>
+      <div>
+        <p>AIBHI AND MONIKA</p>
+      </div>
+    </>
+  )
+}
+
+export default About
